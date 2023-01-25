1d4ebc33355168b761871ae97333e026e22db673
Petrachkova <disprove69@gmail.com>
2023-01-25 11:13:55 +0300

    Add DZ001

---
+++ DZ001/Example001_Max/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите числа");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (number2>number1)
+{
+    max = number2;
+    Console.WriteLine("Второе число больше := " + number2 + " Первое число меньше := " + number1);
+}
+else 
+{
+    Console.WriteLine("Первое число больше := " + number1 + " Второе число меньше := " + number2);
+}
---
+++ DZ001/Example003_Del/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1%2 ==0)
+    Console.WriteLine("Число Чётное");  
+else 
+    Console.WriteLine("Число Нечетное");
